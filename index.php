8de9897c0b97d0173c310b3e32c5f67b9ea735a2
rosedoes <appropriate.business@protonmail.com>
2020-12-16 21:01:16 -0700

    switched to TinyMCE CDN + configured toolbar buttons

--- index.php
+++ index.php
@@ -4,13 +4,6 @@
 	<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<!-- TinyMCE rich text editor -->
-	<script src="tinymce/tinymce.min.js"></script>
-	<!-- Add TinyMCE editor to recipe ingredients textarea -->
-	<script type="text/javascript">
-	tinymce.init({ selector: '#recipePrep' });
-	</script>
-
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
@@ -25,6 +18,20 @@
 		<?php require 'php/lib_add_recipe.php'; ?>
 		<?php require 'php/lib_view_all.php'; ?>
 	</div>
+
+	<!-- TinyMCE rich text editor -->
+	<script src="https://cdn.tiny.cloud/1/qzw4mp5f6fbduulq5sc2gef84ftdrp5q8qcdwe9gl39f6rnq/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+	<!-- Add TinyMCE editor to recipe ingredients textarea -->
+	<script type="text/javascript">
+	tinymce.init({
+		selector: '#recipePrep',
+		menubar: false,
+		plugins: 'lists link',
+	  toolbar: 'bold italic underline strikethrough | numlist bullist link',
+  	link_context_toolbar: true,
+		link_title: false
+	});
+	</script>
 
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
