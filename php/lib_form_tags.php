c25f94ac263fc39c6443dd19002809bb4962b62b
rosedoes <appropriate.business@protonmail.com>
2021-01-11 17:19:04 -0700

    make checkbox labels clickable

--- php/lib_form_tags.php
+++ php/lib_form_tags.php
@@ -1,75 +1,75 @@
 <div class="form-check">
-	<input class="form-check-input" type="checkbox" value="Breakfast" name="recipeTags[]">
+	<input class="form-check-input" type="checkbox" id="tagBreakfast" value="Breakfast" name="recipeTags[]">
 	<label class="form-check-label" for="tagBreakfast">
 		breakfast
 	</label>
 </div>
 
 <div class="form-check">
-	<input class="form-check-input" type="checkbox" value="Dinner" name="recipeTags[]">
+	<input class="form-check-input" type="checkbox" id="tagDinner" value="Dinner" name="recipeTags[]">
 	<label class="form-check-label" for="tagDinner">
 		dinner
 	</label>
 </div>
 
 <div class="form-check">
-	<input class="form-check-input" type="checkbox" value="VanOk" name="recipeTags[]">
+	<input class="form-check-input" type="checkbox" id="tagVanOk" value="Van OK" name="recipeTags[]">
 	<label class="form-check-label" for="tagVanOk">
 		van OK
 	</label>
 </div>
 
 <div class="form-check">
-	<input class="form-check-input" type="checkbox" value="VegOk" name="recipeTags[]">
+	<input class="form-check-input" type="checkbox" id="tagVegOk" value="Vegetarian OK" name="recipeTags[]">
 	<label class="form-check-label" for="tagVegOk">
 		vegetarian OK
 	</label>
 </div>
 
 <div class="form-check">
-	<input class="form-check-input" type="checkbox" value="ChickenOk" name="recipeTags[]">
+	<input class="form-check-input" type="checkbox" id="tagChickenOk" value="Chicken OK" name="recipeTags[]">
 	<label class="form-check-label" for="tagChickenOk">
 		chicken OK
 	</label>
 </div>
 
 <div class="form-check">
-	<input class="form-check-input" type="checkbox" value="Beef" name="recipeTags[]">
+	<input class="form-check-input" type="checkbox" id="tagBeef" value="Beef" name="recipeTags[]">
 	<label class="form-check-label" for="tagBeef">
 		beef
 	</label>
 </div>
 
 <div class="form-check">
-	<input class="form-check-input" type="checkbox" value="BakedGoods" name="recipeTags[]">
-	<label class="form-check-label" for="tagBakedGoods">
-		baked goods
+	<input class="form-check-input" type="checkbox" id="tagSweets" value="Sweets" name="recipeTags[]">
+	<label class="form-check-label" for="tagSweets">
+		sweets and desserts
 	</label>
 </div>
 
 <div class="form-check">
-	<input class="form-check-input" type="checkbox" value="NoBake" name="recipeTags[]">
+	<input class="form-check-input" type="checkbox" id="tagNoBake" value="No Bake" name="recipeTags[]">
 	<label class="form-check-label" for="tagNoBake">
 		no-bake
 	</label>
 </div>
 
 <div class="form-check">
-	<input class="form-check-input" type="checkbox" value="Cookies" name="recipeTags[]">
+	<input class="form-check-input" type="checkbox" id="tagCookies" value="Cookie" name="recipeTags[]">
 	<label class="form-check-label" for="tagCookies">
 		cookies
 	</label>
 </div>
 
 <div class="form-check">
-	<input class="form-check-input" type="checkbox" value="Bread" name="recipeTags[]">
+	<input class="form-check-input" type="checkbox" id="tagBread" value="Bread" name="recipeTags[]">
 	<label class="form-check-label" for="tagBread">
 		bread
 	</label>
 </div>
 
 <div class="form-check">
-	<input class="form-check-input" type="checkbox" value="Favorites" name="recipeTags[]">
+	<input class="form-check-input" type="checkbox" id="tagFavorites" value="Favorite" name="recipeTags[]">
 	<label class="form-check-label" for="tagFavorites">
 		favorites
 	</label>
